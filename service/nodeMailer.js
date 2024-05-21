e716abab1294c9a79660aa9f472008e35e81ec50
vue2023W2 <lobinda123@gmail.com>
2024-05-21 11:57:02 +0800

    update

---
+++ service/nodeMailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'capital-bus.com.tw',
+  port: 25,  // SMTP port
+  secure: true,  // true for 465, false for other ports
+  auth: {
+    user: 'sodu_service@capital-bus.com.tw',  // your email address
+    pass: 'cPsS9620',  // your email password
+  },
+  tls: {
+    rejectUnauthorized: false  // do not fail on invalid certs
+  }
+});
+
+const mailOptions = {
+  from: 'sodu_service@capital-bus.com.tw',
+  to: 'winer909@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+const sendMail = () => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        resolve(info.response);
+      }
+    });
+  });
+};
+
+module.exports = { sendMail };
