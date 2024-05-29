ff39c33c0ae2d176bdc6cc5fea9bfafbb537912c
vue2023W2 <lobinda123@gmail.com>
2024-05-29 09:40:04 +0800

    update

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,8 +1,13 @@
 module.exports = {
-    service: 'gmail',
-    auth: {
-      user: 'your_email@gmail.com',
-      pass: 'your_email_password',
-    },
+  host: 'capital-bus.com.tw',
+  port: 25,  // SMTP port
+  secure: false,  // true for 465, false for other ports
+  auth: {
+    user: 'sodu_service@capital-bus.com.tw',  // your email address
+    pass: 'cPsS9620',  // your email password
+  },
+  tls: {
+    rejectUnauthorized: false  // do not fail on invalid certs
+  }
   };
   
